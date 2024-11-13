3c431f1210dbb8566b37d422fb3c10668ce3c7fa
Johannes Maier <johannes2.maier@tum.de>
2024-11-14 07:06:40 +1100

    Derive Clone and Copy for Instruction

--- src/instruction.rs
+++ src/instruction.rs
@@ -25,7 +25,7 @@ use zicsr_extension::ZicsrOpcode;
 use zifencei_extension::ZifenceiOpcode;
 
 /// Instruction
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 pub struct Instruction {
     /// Opcode
     pub opc: OpcodeKind,
@@ -265,7 +265,7 @@ fn reg2str(rd_value: usize) -> &'static str {
 
 /// Instruction format
 /// See: [The RISC-V Instruction Set Manual: Volume II Version 20240411](https://github.com/riscv/riscv-isa-manual/releases/download/20240411/priv-isa-asciidoc.pdf) p.23,141
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 pub enum InstFormat {
     /// Regular format
     /// ```ignore
@@ -436,7 +436,7 @@ pub trait Opcode {
 }
 
 /// Extension type and Instruction name.
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 pub enum OpcodeKind {
     /// Base Integer Instruction Set
     BaseI(BaseIOpcode),
